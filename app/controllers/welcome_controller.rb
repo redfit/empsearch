11895070c76078ddb9851e50fee853eb7943b4c5
redfit <daisuke.fuchiwaki@excel.co.jp>
2012-06-27 02:25:32 +0900

    mercury 微調整

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,6 +7,8 @@ class WelcomeController < ApplicationController
     page = Page.find(params[:id])
     page.title = params[:content][:page_title][:value]
     page.description = params[:content][:page_description][:value]
+    page.content1 = params[:content][:page_content1][:value]
+    page.content2 = params[:content][:page_content2][:value]
     page.save!
     render text: ""
   end
